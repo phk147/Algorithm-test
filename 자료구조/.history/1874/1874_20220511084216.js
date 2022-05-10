cfbdf66c642a213f45b166c6e5771230a418b460
parkhyeonki <phk147@gmail.com>
2022-05-11 08:58:27 +0900

    2022.05.11 add 1874

---
+++ 자료구조/.history/1874/1874_20220511084216.js
@@ -0,0 +1,46 @@
+/**
+ * 제출용. 아래 로컬용을 지우고 제출하자.
+ */
+//  let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
+/**
+ * 로컬용, 예제.txt를 생성해서 예제를 복붙하자.
+ */
+
+function solution() {
+  let input = require("fs")
+    .readFileSync("예제.txt")
+    .toString()
+    .trim()
+    .split("\n")
+    .map((val) => val.trim());
+
+  let testNum = input.shift();
+  let stack = [];
+  let answer = [];
+  input.forEach((num) => {
+    if (stack.indexOf(num) === -1) {
+      for (let i = stack[stack.length - 1]; i <= num; i++) {
+        stack.push(i);
+        answer.push("+");
+        if (i === num) {
+          answer.push("+");
+          answer.push("-");
+        }
+      }
+    } else if (stack.indexOf(num) === num - 1) {
+      stack.pop();
+      answer.push("-");
+    } else {
+      for (let j = stack.length - 1; j >= stack.indexOf(num); j--) {
+        stack.pop();
+        answer.push("-");
+      }
+    }
+  });
+
+  console.log(input, testNum);
+}
+
+solution();
+
+//[4,3,6,8,7,5,2,1]
