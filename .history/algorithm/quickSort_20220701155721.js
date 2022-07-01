5839be17708bd36c4b290d7498ed5b6c1392f3e3
parkhyeonki <phk147@gmail.com>
2022-07-01 19:19:14 +0900

    baekjoon 15649

---
+++ .history/algorithm/quickSort_20220701155721.js
@@ -0,0 +1,21 @@
+let arr = [5, 1, 3, 6, 4, 8, 9, 7, 2, 5, 6];
+function quickSort(arr, start, end) {
+  if (start >= end) return;
+  let pivot = start;
+  let left = start + 1;
+  let right = end;
+  while (left <= right) {
+    //left pivot보다 큰 수 찾기
+    while (left <= end && arr[left] <= arr[pivot]) left++;
+    //right pivot보다 작은 수 찾기
+    while (right > start && arr[right] > arr[pivot]) right--;
+    //left,right가 엇갈리는 순간 pivot 변경
+    if (left > right) [arr[pivot], arr[right]] = [arr[right], arr[pivot]];
+    else [arr[left], arr[right]] = [arr[right], arr[left]];
+  }
+  quickSort(arr, start, right - 1);
+  quickSort(arr, right + 1, end);
+}
+
+quickSort(arr, 0, arr.length);
+console.log(arr.length);
---
+++ .history/baekjoon/15649/javascript_20220701101642.js
@@ -0,0 +1,38 @@
+/**
+ * 제출용. 아래 로컬용을 지우고 제출하자.
+ */
+//  let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
+/**
+ * 로컬용, 예제.txt를 생성해서 예제를 복붙하자.
+ */
+let input = require("fs")
+  .readFileSync("input.txt") //"/dev/stdin"
+  .toString()
+  .split("\n")
+  .map((val) => val.trim());
+
+let [n, m] = input.shift().split(" ");
+function solution() {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+
+  let P = Permutation(arr, m);
+
+  P = P.map((v) => v.join("").split(",").join(""));
+
+  console.log(P);
+}
+function Permutation(arr, r) {
+  const result = [];
+  if (r === 1) return arr.map((num) => [num]);
+  arr.forEach((fixed, index, org) => {
+    const rest = [...org.slice(0, index), ...org.slice(index + 1)];
+    const permutation = Permutation(rest, r - 1);
+    const attached = permutation.map((numbers) => [fixed, ...numbers]);
+    result.push(...attached);
+  });
+  return result;
+}
+solution();
