1ed73d5a77c0539948ffebf6037df0de6414101d
parkhyeonki <phk147@gmail.com>
2023-02-10 16:45:36 +0900

    programmers nqeen

--- programmers/nQueen/javascript.js
+++ programmers/nQueen/javascript.js
@@ -6,27 +6,41 @@ function solution() {
     .map((val) => val.trim());
 
   let n = +input.shift();
+  let ans = 0;
 
-  let graph = new Array(n).fill().map((v) => new Array(n).fill(0));
+  let vis = new Array(n + 1).fill(0);
 
   // Q은 가로 세로 대각선으로 이동가능
   // n은 12 이하의 자연수
   // Q을 배치하고 가로세로 대각선을 확인?
 
-  const queen = (graph, n) => {
-    if (n === 0) return;
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n; j++) {
-        if (graph[i][j] === 0) {
-          graph[i][j] = 1;
+  for (let i = 0; i < n; i++) {
+    vis[0] = i;
+    dfs(1);
+  }
+
+  function dfs(row) {
+    if (n === row) {
+      ans += 1;
+    }
+    for (let col = 0; col < n; col++) {
+      vis[row] = col;
+      let checker = true;
+      for (let i = 0; i < row; i++) {
+        if (vis[row] === vis[i]) {
+          checker = false;
+          break;
+        }
+        if (Math.abs(vis[row] - vis[i]) === Math.abs(i - row)) {
+          checker = false;
           break;
         }
       }
+      if (checker) dfs(row + 1);
     }
-    queen(graph, n - 1);
-  };
+  }
 
-  console.log(graph);
+  console.log(ans);
 }
 
 solution();
