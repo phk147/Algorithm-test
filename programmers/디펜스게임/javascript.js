fc3c71bcd7eedbdca70796f04baa6365f1d5ac58
parkhyeonki <phk147@gmail.com>
2023-02-17 15:37:27 +0900

    programmers 디펜스게임

--- programmers/디펜스게임/javascript.js
+++ programmers/디펜스게임/javascript.js
@@ -5,64 +5,42 @@ function solution() {
     .split("\n")
     .map((val) => val.trim());
 
-  let [n,k] = input.shift().split(" ").map(v=>+v);
-  let enemy = input.shift().split(" ").map(v=>+v);
-
+  let [n, k] = input
+    .shift()
+    .split(" ")
+    .map((v) => +v);
+  let enemy = input
+    .shift()
+    .split(" ")
+    .map((v) => +v);
 
   // n ->life k->pass
 
-  let answer = 0;
-
-  if(k>=enemy.length) return enemy.length;
-
-  let psum = 0;
-  let pass = new Array(k).fill(-1);
-
-  for(let i=0;i<enemy.length;i++){
-    let numE = enemy[i];
-
-    if(psum+numE <=n){
-      psum+=numE;
-      answer++;
-
-      // 큰 수를 저장해둔다
+  let [left, right] = [0, enemy.length];
+  let mid = Math.floor((left + right) / 2);
+  while (left <= right) {
+    // 해당 탐색에서 사용될 길이 내림차 순(k 소진을 위함)
+    const curSlice = enemy.slice(0, mid).sort((a, b) => b - a);
+    let noDie = k;
+    // 전쟁 후 남을 상대 병사의 수
+    const curEnemy = curSlice.reduce((acc, cur) => {
+      // 무적권이 있다면
+      if (noDie > 0) {
+        noDie--;
+        return acc;
+      }
+      return acc + cur;
+    }, 0);
+    // 상대 병사를 무적권 한도 내에 모두 무찌를 수 있는가?
+    if (n - curEnemy >= 0 && noDie >= 0) {
+      left = mid + 1;
     } else {
-     
-      console.log("!",i);
-      pass.sort((a,b)=>a-b);
-      while(numE+psum > n&&pass.length){
-        let temp = pass.pop();
-        if(temp === -1) {answer++; break;}
-        psum-=temp;
-        answer++;
-      }
-      
-      if(!pass.length&&numE+psum >n) break;
-      
-      psum+=numE;
-      
-
+      right = mid - 1;
     }
-
-    if(pass.length){
-
-      for(let j=0;j<pass.length;j++){
-        if(pass[j]<numE) {pass[j]=numE; break;}
-      }
-    }
-
-    console.log("-----------------")
-    console.log("i",i);
-    console.log("answer",answer);
-    console.log("numE",numE);
-    console.log("psum",psum);
-    console.log("pass",pass);
-    console.log("-----------------")
-
-    
+    mid = Math.floor((left + right) / 2);
   }
-
-  console.log(answer);
+  console.log(left - 1);
+  return left - 1;
 }
 
 solution();
