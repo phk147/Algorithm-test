60592efec798fee5c96bb5d9f23d156b85b2818c
parkhyeonki <phk147@gmail.com>
2023-03-30 11:49:32 +0900

    baekjoon 21608

--- baekjoon/21608/javascript.js
+++ baekjoon/21608/javascript.js
@@ -12,7 +12,7 @@ function solution() {
     .split("\n")
     .map((val) => val.trim());
 
-  let score = [0, 1, 10, 100, 100];
+  let score = [0, 1, 10, 100, 1000];
 
   let n = +input.shift();
   let students = input.map((v) => v.split(" ").map((val) => +val));
@@ -22,10 +22,9 @@ function solution() {
   let queue = new Array(n * n + 1).fill(0);
   let dx = [0, 1, 0, -1];
   let dy = [1, 0, -1, 0];
+
   for (let i = 0; i < students.length; i++) {
     let [student, ...like] = students[i];
-    console.log(student, like);
-    console.log("graph", graph);
     if (i === 0) {
       let mid = Math.floor(n / 2);
       graph[mid][mid] = student;
@@ -44,44 +43,68 @@ function solution() {
         let cnt = 0;
         let likeCnt = 0;
         for (let j = 0; j < 4; j++) {
-          let checkX = nx + dx[dir];
-          let checkY = ny + dy[dir];
+          let checkX = nx + dx[j];
+          let checkY = ny + dy[j];
           if (checkX < 0 || checkX >= n || checkY < 0 || checkY >= n) continue;
           if (graph[checkX][checkY] === 0) cnt++;
           if (like.indexOf(graph[checkX][checkY]) !== -1) likeCnt++;
         }
+
+        if (temp.filter((v) => v[2] === nx && v[3] === ny).length) continue;
         temp.push([likeCnt, cnt, nx, ny]);
       }
     }
-    console.log("temp", temp);
 
+    let maxEmpty = -1;
+    let maxLike = -1;
+    for (let a = 0; a < temp.length; a++) {
+      maxEmpty = Math.max(maxEmpty, temp[a][1]);
+      maxLike = Math.max(maxLike, temp[a][0]);
+    }
+
+    console.log(student, temp);
+    temp = temp.filter((v) => v[0] === maxLike);
     // 1. 비어있는 칸 중에서 좋아하는 학생이 인접한 칸에 가장 많은 칸으로 자리를 정한다.
-    if (temp.filter((v) => v[0] !== 0).length === 1) {
+    if (temp.length === 1) {
       temp.sort((a, b) => b[0] - a[0]);
       graph[temp[0][2]][temp[0][3]] = student;
       queue[student] = [temp[0][2], temp[0][3]];
       continue;
     }
 
+    temp = temp.filter((v) => v[1] === maxEmpty);
     // 2. 1을 만족하는 칸이 여러개이면, 인접한 칸 중에서 비어있는 칸이 가장 많은 칸으로 정한다.
-    let max = -1;
-    for (let a = 0; a < temp.length; a++) {
-      max = Math.max(max, temp[1]);
-    }
-    if (temp.filter((v) => v[0] === max).length === 1) {
+    if (temp.length === 1) {
       temp.sort((a, b) => b[1] - a[1]);
       graph[temp[0][2]][temp[0][3]] = student;
       queue[student] = [temp[0][2], temp[0][3]];
       continue;
     }
-
     // 3. 2를 만족하는 칸도 여러개인 경우에는 행의 번호가 가장 작은 칸으로, 그러한 칸도 여러개이면 열의 번호가 가장 작은 칸
     temp.sort((a, b) => a[2] - b[2] || a[3] - b[3]);
     graph[temp[0][2]][temp[0][3]] = student;
     queue[student] = [temp[0][2], temp[0][3]];
   }
-  console.log(graph);
+  const likeCheck = (x, y) => {
+    let cnt = 0;
+    let student = graph[x][y];
+    let like = students.filter((v) => v[0] === student)[0].slice(1);
+    for (let dir = 0; dir < 4; dir++) {
+      let nx = x + dx[dir];
+      let ny = y + dy[dir];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+      if (like.indexOf(graph[nx][ny]) !== -1) cnt++;
+    }
+    return score[cnt];
+  };
+  let answer = 0;
   // 만족도-> 그 학생과 인접한 칸에 앉은 좋아하는 학생의 수
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      answer += likeCheck(i, j);
+    }
+  }
+  console.log(answer);
 }
 
 solution();
